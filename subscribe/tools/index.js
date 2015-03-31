4b8f42c1876c83df0af73f8c4f723595c78825f8
Antonio Pellegrino <pellepelle3@gmail.com>
2015-03-31 17:45:54 -0400

    add readme on public dir

--- subscribe/tools/index.js
+++ subscribe/tools/index.js
@@ -4,7 +4,7 @@ var tools = {
 	, getReportingInfo : require('./reporting')
 	, healthCheck : require('./healthCheck')
 	, test : require('./test')	
-	, homepage : require('./homePage')	
+	, homePage : require('./homePage')	
 }
 
 module.exports = tools
