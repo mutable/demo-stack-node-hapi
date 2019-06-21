aaee6f28099ef6cd2170f8511194354aa6656c23
Hasmik <hasmikatomyan@gmail.com>
2019-06-21 13:57:48 +0400

    Update service-sendemail/api/index.js
    Co-Authored-By: Vache Asatryan <vache@mutable.io>

--- service-sendemail/api/index.js
+++ service-sendemail/api/index.js
@@ -14,7 +14,7 @@ API.get = (req) => {
   console.log(limit)
   const parameters = [
     offset ? offset : 0,
-    limit ? limit : 0
+    limit ? limit : 10
   ];
   console.log(parameters)
   return  Object.assign({}, {'parameters': parameters, 'message': 'you made a request'});
